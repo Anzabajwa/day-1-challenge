97fff8ce4660c356f19e252db4c04b812a8bcf77
ANZA BAJWA <161083667+Anzabajwa@users.noreply.github.com>
2024-03-15 07:21:44 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+//Day 01
+//QUESTION1
+var personName = " Asharib";
+console.log("Hello ".concat(personName, " would you like to learn some typescript today?"));
+//QUESTION2
+var myName = " Anza sarwar";
+console.log(myName.toLowerCase());
+console.log(myName.toUpperCase());
+console.log(myName.replace(/\b\w/g, function (char) { return char.toUpperCase(); }));
---
+++ main.ts
@@ -0,0 +1,16 @@
+//Day 01
+//QUESTION1
+
+
+let personName : string = " Asharib";
+console.log(`Hello ${personName} would you like to learn some typescript today?`);
+
+
+//QUESTION2
+let myName : string = " Anza sarwar";
+console.log(myName.toLowerCase());
+
+console.log(myName.toUpperCase());
+
+console.log( myName.replace   (/\b\w/g,    char => char.toUpperCase()));
+
